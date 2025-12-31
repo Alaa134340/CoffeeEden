a5a2ca5ddea59c8d30de9fba956e6bb10114977b
Alaa134340 <alaa.mheich8@gmail.com>
2026-01-01 00:02:39 +0200

    Fix config.js API URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,4 @@
 // API Configuration
-// Change this URL based on your environment
-const API_URL = process.env.REACT_APP_API_URL || process.env.NODE_ENV === 'production' ? 'https://coffeeeden.onrender.com' : 'http://localhost:5000';
+const API_URL = 'https://coffeeeden.onrender.com';
 
 export default API_URL;
